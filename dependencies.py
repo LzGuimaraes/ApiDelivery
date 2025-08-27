42007c9c2b3d7a8f0b923fca043159ba066efb9d
LzGuimares <luizsantosleventi19901@gmail.com>
2025-08-27 16:40:37 -0400

    Autenticalçao de Token via formulario + autenticação com JWT

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,8 @@
-from models import db
-from sqlalchemy.orm import sessionmaker
+from fastapi import Depends, HTTPException
+from main import SECRET_KEY, ALGORITHM, oath2_schema
+from models import db, Usuario
+from sqlalchemy.orm import sessionmaker, Session
+from jose import jwt, JWTError
 
 def pegar_sessao():
     try:
@@ -8,3 +11,14 @@ def pegar_sessao():
         yield session
     finally:
         session.close()
+
+def verificar_token(token: str = Depends(oath2_schema), session: Session = Depends(pegar_sessao)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id_usuario = int (dic_info.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail= "Acesso Negado")
+    usuario = session.query(Usuario).filter(Usuario.id==id_usuario).first()
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso Invalido")
+    return usuario
